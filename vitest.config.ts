51d778da3eb673924f37ebc92128f2223e2b6db5
Hana <momenthana@kakao.com>
2023-01-23 14:17:20 +0900

    fix: vitest의 vite 의존성을 변경

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from 'url'
 
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [react()],
@@ -16,4 +16,4 @@ export default defineConfig({
     globals: true,
     setupFiles: './__tests__/setup.ts',
   },
-});
+})
