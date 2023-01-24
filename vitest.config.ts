c1c00d3fcc63be88c625aef1b3fbf4862128ff56
Hana <momenthana@kakao.com>
2023-01-24 15:11:58 +0900

    feat: vitest lambda .graphql loader

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,16 @@
 import { fileURLToPath, URL } from 'url'
 
 import react from '@vitejs/plugin-react-swc'
+import graphql from '@rollup/plugin-graphql'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    graphql({
+      exclude: ['**/*.gql'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./', import.meta.url)),
