06bb6f4379aa770d4e1ca9636ca9d6b1c23eee7d
vivi <vivi@onuii.com>
2023-02-21 14:49:24 +0900

    chore: seoltab eslint 설치

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { ApolloProvider } from '@apollo/client';
 import { initializeApp } from 'firebase/app';
 import type { AppProps } from 'next/app';
 import { useEffect } from 'react';
+
 import { useApollo } from '@/lib/gql/apollo';
 
 const firebaseConfig = {
